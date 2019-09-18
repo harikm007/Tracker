350737743c11b7661a6de05ada00ffbd3b9b6b16
Hari KM <Hari.KM@buck.com>
2019-09-18 15:02:53 +0530

    Test Git integration from Visual Studio

---
+++ Tracker/Model/Project.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tracker.Model
+    [Table("Project")]
+    public class Project
+    {
+        public int ProjectID { get; set; }
+        public string ProjectName { get; set; }
+        public int ProjectLocationId { get; set; }
+        public string ProjectStatusId { get; set; }
+        public string Description { get; set; }
+
+    }
+}
---
+++ Tracker/Model/ProjectEmployeeMapping.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tracker.Model
+{
+    public class ProjectEmployeeMapping
+    {
+    }
+}
